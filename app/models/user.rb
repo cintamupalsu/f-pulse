cc8d59b70d9c8b09d5176d8cccbdbdc6b1f1daa2
a_maulana <cintamupalsu@hotmail.com>
2021-04-14 17:11:35 +0900

    remove restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
-    return nil unless email =~ /@sbs-infosys.co.jp\z/
+    #return nil unless email =~ /@sbs-infosys.co.jp\z/
     create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
