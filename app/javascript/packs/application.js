6b75cb7306fa8345bf7712a6677bf7122d8dd87d
a_maulana <cintamupalsu@hotmail.com>
2021-04-15 13:20:46 +0900

    Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
-import '../stylesheets/customs'
+import '../stylesheets/custom'
 require('jquery')
 
 
