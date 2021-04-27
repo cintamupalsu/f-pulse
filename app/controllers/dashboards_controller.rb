96bb00933d26b8e350ab2773cfe38e9a8e1c59ab
a_maulana <cintamupalsu@hotmail.com>
2021-04-27 16:44:53 +0900

    fix user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,9 @@
 class DashboardsController < ApplicationController
-  #before_action :authenticate_user!  #unremark
+  before_action :authenticate_user!  #unremark
   
   def show
     @user = current_user
-    @user = User.first # remark
+    #@user = User.first # remark
   end
 
 end
--- app/controllers/mujins_controller.rb
+++ app/controllers/mujins_controller.rb
@@ -1,12 +1,12 @@
 class MujinsController < ApplicationController
-    #before_action :authenticate_user!  #unremark
+    before_action :authenticate_user!  #unremark
     
     def show #Mujin dashboard
        
     end
 
     def management
-        current_user = User.first #remark😀
+        #current_user = User.first #remark😀
         @mujins = current_user.mujins
     end
     
