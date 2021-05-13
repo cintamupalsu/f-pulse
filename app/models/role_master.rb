c391e7f21bea6f91b78db5ebcdc88514402f9f44
a_maulana <cintamupalsu@hotmail.com>
2021-05-13 19:18:26 +0900

    d destroy 2

--- app/models/role_master.rb
+++ app/models/role_master.rb
@@ -2,6 +2,6 @@ class RoleMaster < ApplicationRecord
     validates :content, presence: true, length: { maximum: 50 }
     validates :abrev, presence: true, length: { maximum: 5 }
     validates :description, presence: true, length: { maximum: 500 }
-    has_many :role_transactions
+    has_many :role_transactions, dependent: :destroy
     has_many :role_users, dependent: :destroy
 end
