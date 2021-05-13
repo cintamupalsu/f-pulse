8183ab21314aea1cfb56d32cecc530b394f45ab7
a_maulana <cintamupalsu@hotmail.com>
2021-05-13 19:22:22 +0900

    dependent destroy 03

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: [:google_oauth2]
-  has_many :mujins
-  has_many :role_users
+  has_many :mujins, dependent: :destroy
+  has_many :role_users, dependent: :destroy
 
   def self.from_google(email:, full_name:, uid:, avatar_url:)
     #return nil unless email =~ /@sbs-infosys.co.jp\z/
