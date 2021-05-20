f647be24936b25a7bc93b87e7780b59de47062f8
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-05-20 11:23:01 +0900

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Create Features
-FeatureMaster.create!(content: "Role Menu", abrev: "00001", master: true)
-FeatureMaster.create!(content: "ユーザーﾏｽﾀｰ", abrev: "00002", master: true)
-FeatureMaster.create!(content: "作業内容ﾏｽﾀｰ", abrev: "00003", master: true)
-FeatureMaster.create!(content: "農作物ﾏｽﾀｰ", abrev: "00004", master: true)
-FeatureMaster.create!(content: "ボランティア機能", abrev: "00005", master: false)
+FeatureMaster.create!(content: "Role Menu", abrev: "00100", master: true)
+FeatureMaster.create!(content: "ユーザーﾏｽﾀｰ", abrev: "00200", master: true)
+FeatureMaster.create!(content: "作業内容ﾏｽﾀｰ", abrev: "00300", master: true)
+FeatureMaster.create!(content: "農作物ﾏｽﾀｰ", abrev: "00400", master: true)
+FeatureMaster.create!(content: "ボランティア機能", abrev: "00500", master: false)
 
 
 
