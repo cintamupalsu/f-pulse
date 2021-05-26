9af65fa659cd8b6b3fb7b73a61f20aa4b0e9bb69
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-05-26 09:21:10 +0900

    grant access

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Create Features
-# FeatureMaster.create!(content: "Role Menu", abrev: "00100", master: true)
-# FeatureMaster.create!(content: "ユーザーﾏｽﾀｰ", abrev: "00200", master: true)
-# FeatureMaster.create!(content: "作業内容ﾏｽﾀｰ", abrev: "00300", master: true)
-# FeatureMaster.create!(content: "農作物ﾏｽﾀｰ", abrev: "00400", master: true)
-# FeatureMaster.create!(content: "ボランティア機能", abrev: "00500", master: false)
+FeatureMaster.create!(content: "Role Menu", abrev: "00100", master: true)
+FeatureMaster.create!(content: "ユーザーﾏｽﾀｰ", abrev: "00200", master: true)
+FeatureMaster.create!(content: "作業内容ﾏｽﾀｰ", abrev: "00300", master: true)
+FeatureMaster.create!(content: "農作物ﾏｽﾀｰ", abrev: "00400", master: true)
+FeatureMaster.create!(content: "ボランティア機能", abrev: "00500", master: false)
 
 fm = FeatureMaster.find(1)
 fm.sub_feature_masters.create!(content: "追加", abrev: "00101" )
