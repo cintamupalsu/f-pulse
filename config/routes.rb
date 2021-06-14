ec4702acb31fccc2bce1b3d3a98f794e5c2f25ab
アリフ　マウラナ <a_maulana@sbs-infosys.co.jp>
2021-06-14 11:50:02 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'role_masters/new'
   #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'dashboards#show'
+  #root to: 'dashboards#show'
+  root to: 'mujins#mgmt'
   get '/master_maintenance', to: 'dashboards#masters_maintenance'
   get '/user_index', to: 'dashboards#user_index'
   get '/user_role', to: 'dashboards#user_role'
